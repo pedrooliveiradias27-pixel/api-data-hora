4c608c38605c742c03f33768e7b1064617b16249
pedrooliveiradias27 <pedrooliveiradias27@gmail.com>
2026-08-27 17:05:55 -0300

    corriginde teste

--- test/app.test.js
+++ test/app.test.js
@@ -6,6 +6,6 @@ describe("API Data e Hora", () => {
     test("GET /deve retornar HTTPS 200", async () => {
         const response = await request(app).get("/");
 
-        expect(response.statusCode).toBe(201);
+        expect(response.statusCode).toBe(200);
     });
 });
